f6dec68681c3e3ca9b251eb3d3c7a0117374edc0
Nishant Sapkota <snishant306@gmail.com>
2021-03-21 21:49:43 +0545

    ok

---
+++ script.js
@@ -0,0 +1,19 @@
+function MyFunction()
+    {
+        document.getElementById("name").innerHTML = "Not Nishant Sapkota";
+        document.getElementById("name").style.color = "wheat";
+        document.getElementById("name").style.fontFamily = "Cascadia Code";
+        document.getElementById("dp").src = 'unknown.jpg';
+        document.getElementById("mailbt").innerHTML = "randomemail@email.com";
+        alert("Changed!");
+    }
+
+function MyFunction1()
+    {
+        document.getElementById("name").innerHTML = "Nishant Sapkota";
+        document.getElementById("name").style.color = "gold";
+        document.getElementById("name").style.fontFamily = "Arvo";
+        document.getElementById("dp").src = 'dp.jpg';
+        document.getElementById("mailbt").innerHTML = "snishant306@gmail.com";
+        alert("Reverted!")
+    }
