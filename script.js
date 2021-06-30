aeeca1805e7b418ad22585bf3b22344981576f8f
Nishant Sapkota <snishant306@gmail.com>
2021-06-30 14:10:09 +0545

    Removed somethings

--- script.js
+++ script.js
@@ -6,22 +6,7 @@ function MyFunction()
         document.getElementById("name").style.fontFamily = "Cascadia Code";
         document.getElementById("dp").src = 'https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif';
         document.getElementById("mailbt").innerHTML = "randomemail@email.com";
-        alert("Changed!");
+        //alert("Changed!");
         sound = document.getElementById('lol');
         sound.play();
-    }
-
-function MyFunction1()
-    {
-        document.getElementById("name").innerHTML = "Nishant Sapkota";
-        document.getElementById("text").innerHTML = "Hello there! This is me. I love computers, books, nature, space. Love to spend time alone infront of my Computer.\
-        Currently Studying BE Computer in MBMAN.Built this site to test my HTML and CSS skills. Turns out I am not as good as I thought I was. :("
-        document.getElementById("name").style.color = "gold";
-        document.getElementById("name").style.fontFamily = "Arvo";
-        document.getElementById("dp").src = 'dp.jpg';
-        document.getElementById("mailbt").innerHTML = "snishant306@gmail.com";
-        alert("Reverted!")
-        sound = document.getElementById('lol');
-        sound.pause();
-        sound.currentTime = 0;
     }
