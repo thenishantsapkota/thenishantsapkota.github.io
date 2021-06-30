2ae381eddd476e83be2fd1052c8f33ac83d61593
Nishant Sapkota <snishant306@gmail.com>
2021-06-30 14:14:48 +0545

    Ok

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function MyFunction()
     {
         document.getElementById("name").innerHTML = "RickRoll'd";
         document.getElementById("name").style.color = "wheat";
-        document.getElementById("text").innerHTML  =  "You got rickrolled. Ha! Yet again"
+        document.getElementById("text").innerHTML  =  "LMAO! Make your CV yourself."
         document.getElementById("name").style.fontFamily = "Cascadia Code";
         document.getElementById("dp").src = 'https://media.giphy.com/media/Vuw9m5wXviFIQ/giphy.gif';
         document.getElementById("mailbt").innerHTML = "randomemail@email.com";
